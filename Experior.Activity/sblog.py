b60c0db88a7bbb69958715f17f799efbc7ec056a
Tim McNamara <paperless@timmcnamara.co.nz>
2010-08-14 22:29:56 +1200

    Encoding, shebang & licence fixes
     - Corrected encoding declarations
        # encoding: utf-8 => # -*- encoding: utf-8 -*-
     - Corrected the incorrect shebangs
        #!/usr/env/bin python => #! /usr/env/bin python
     - removed licence boilerplate from docstrings

--- Experior.Activity/sblog.py
+++ Experior.Activity/sblog.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-sblog.py
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
 
-This file is part of sugarbot.
+#sblog.py
 
-sugarbot is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+#This file is part of sugarbot.
 
-sugarbot is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+#sugarbot is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
 
-You should have received a copy of the GNU General Public License
-along with sugarbot.  If not, see <http://www.gnu.org/licenses/>.
-"""
+#sugarbot is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with sugarbot.  If not, see <http://www.gnu.org/licenses/>.
+
 import logging
 
 log = logging.getLogger("")
